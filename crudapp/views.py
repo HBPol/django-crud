54d88d49552ab14265119592a458ab9b0d8ac36d
hbp_01 <hector.bolsa@outlook.com>
2018-11-18 22:22:41 +0000

    Creating the views and templates.

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from crudapp.models import Member
+from requests.api import request
 
-# Create your views here.
+def index(request):
+    members = Member.object.all()
+    context = {'members': members}
+    return render(request, 'crudapp/index.html', context)
+
+def create(request):
+    member = Member(firstname=request.POST['firstname'], lastname=request.POST['lastname'])
+    member.save()
+    return redirect('/')
+
+def edit(request, id):
+    members = Member.objects.get(id=id)
+    context = {'members': members}
+    return render(request, 'crudapp/edit.html', context)
+
+def update(request, id):
+    member = Member.objects.get(id=id)
+    member.firstname = request.POST['firstname']
+    member.lasttname = request.POST['lastname']
+    member.save()
+    return redirect('/crudapp/')
+
+def delete(request, id):
+    member = Member.objects.get(id=id)
+    member.delete()
+    return redirect('/crudapp/')
+    
